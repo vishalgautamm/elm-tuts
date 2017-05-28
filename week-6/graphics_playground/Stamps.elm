8fef95c304cee9195b5bb2e88079f929dbc9a5eb
vishalgautamm <vishal.g@live.ca>
2017-05-28 12:07:56 -0400

    Added the main function

--- week-6/graphics_playground/Stamps.elm
+++ week-6/graphics_playground/Stamps.elm
@@ -5,9 +5,29 @@ import Collage exposing (..)
 import Element exposing (..)
 import Html exposing (..)
 
-drawStamp : (Int, Int) -> Form
-drawStamp (x, y) =
-  ngon 5 50
-  |> filled red
-  |> move (toFloat(x), toFloat(-1 *y))
 
+drawStamp : ( Int, Int ) -> Form
+drawStamp ( x, y ) =
+    ngon 5 50
+        |> filled red
+        |> move ( toFloat (x), toFloat (-1 * y) )
+
+-- VIEW 
+view : List ( Int, Int ) -> Element
+view positions =
+    let
+        theGroup =
+            group (List.map drawStamp positions)
+
+        originGroup =
+            move ( -400, 400 ) theGroup
+    in
+        collage 800
+            800
+            [ originGroup ]
+
+-- MAIN
+main : Html msg
+main =
+  view [(0, 0), (100, 100), (200, 200)]
+    |> Element.toHtml
