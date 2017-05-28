b3dcc69ab4de3fcea25af4bf079627b7f415ae8c
vishalgautamm <vishal.g@live.ca>
2017-05-28 12:03:21 -0400

    Added drawStamp function

--- week-6/graphics_playground/Stamps.elm
+++ week-6/graphics_playground/Stamps.elm
@@ -5,3 +5,9 @@ import Collage exposing (..)
 import Element exposing (..)
 import Html exposing (..)
 
+drawStamp : (Int, Int) -> Form
+drawStamp (x, y) =
+  ngon 5 50
+  |> filled red
+  |> move (toFloat(x), toFloat(-1 *y))
+
