abc7e49ec25d46c32d3405125c8e62b6b967a667
vishalgautamm <vishal.g@live.ca>
2017-06-04 11:02:41 -0400

    Added Main and About component

---
+++ week-14/content-catalog/src/About.elm
@@ -0,0 +1,8 @@
+module About exposing (..)
+
+import Html exposing (Html, text)
+
+view : Html msg
+view =
+  text "Welcome to the about page"
+
--- week-14/content-catalog/src/Main.elm
+++ week-14/content-catalog/src/Main.elm
@@ -2,3 +2,79 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import About exposing (view)
+
+-- MAIN
+
+
+main =
+    Html.program
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    {}
+
+
+type Msg
+    = NoOp
+
+
+init : ( Model, Cmd Msg )
+init =
+    {} ! []
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            model ! []
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ navigationView model
+        , About.view
+        ]
+
+
+navigationView : Model -> Html Msg
+navigationView model =
+    nav []
+        [ ul []
+            [ li [] [ a [ href "#" ] [ text "Home" ] ]
+            , li [] [ a [ href "#" ] [ text "topics" ] ]
+            ]
+        ]
+
+
+currentPageView : Model -> Html Msg
+currentPageView model =
+  text "Home Page"
